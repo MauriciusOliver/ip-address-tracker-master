df44e46f22c233f3cef26633558da4e79d46016d
Mauricius <maauricius.cssurf@hotmail.com>
2024-02-29 18:09:56 -0300

    ...

--- componentes/gettingMapIP.js
+++ componentes/gettingMapIP.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', async function () {
   // Obtém o endereço IP do cliente
   async function getIpAddress() {
     try {
-        const response = await fetch('/componentes/api/getIpData.js');
+        const response = await fetch('/api/getIpData.js');
         const data = await response.json();
         return data.query || 'N/A';
     } catch (error) {
